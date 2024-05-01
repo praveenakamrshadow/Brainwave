7c14e0b651310555a39cc43c7bd5bfd82b2378f6
praveen Sahu <praveen.pkyt327867@gmail.com>
2024-05-01 16:34:53 +0530

    Button component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
+import Button from "./components/Button.jsx";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10">something</Button>
+      </div>
+      <ButtonGradient />
+    </>
+  );
 };
 
 export default App;
